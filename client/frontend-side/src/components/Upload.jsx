bbf822d6c7d77b89c9f8331aa2dbf80ec037192d
Mihir Makwana <makwana.mi@northeastern.edu>
2025-02-17 20:15:14 -0500

    Created 2 divs - Options and Input

--- client/frontend-side/src/components/Upload.jsx
+++ client/frontend-side/src/components/Upload.jsx
@@ -2,7 +2,30 @@ import React from 'react'
 
 const Upload = () => {
   return (
-    <div>Upload</div>
+    <div className='App'>
+        <h1>
+            Medical Image Scanning
+        </h1>
+        <form action="">
+            {/* This div is for options */}
+            <div> 
+                <label>
+                    <select name="" id="">
+                        <option value="MRI">MRI</option>
+                        <option value="XRAY">XRAY</option>
+                    </select>
+                </label>
+            </div>
+
+            {/* This div is for upload image */}
+            <div>
+                <label>
+                    Upload Image:
+                    <input type="file" />
+                </label>
+            </div>
+        </form>
+    </div>
   )
 }
 
